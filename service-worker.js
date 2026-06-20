8f5bf2bf76ba1067c4acbe766971874873348d62
Fidcom <danielrivera@live.com>
2026-06-20 17:08:08 -0400

    Corrige overflow de cards de resultado dentro del contenedor

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "rail-estimator-v13";
+const CACHE_NAME = "rail-estimator-v14";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => {
